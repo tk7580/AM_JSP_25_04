6b3c4ca65f9da2df5d99d98c239b255110ff277f
tk7580 <tkcode7580@gmail.com>
2025-04-23 15:02:08 +0900

    test1

---
+++ src/main/java/com/KoreaIT/java/AM_jsp/PrintDanServlet.java
@@ -0,0 +1,34 @@
+package com.KoreaIT.java.AM_jsp;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/printDan")
+public class PrintDanServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public PrintDanServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html;charset=UTF-8");
+		
+		response.getWriter().append("===8단===<br>");
+		
+		int dan = 8;
+		for (int i = 1; i <= 9; i++) {
+			response.getWriter().append(String.format("%d * % d = %d<br>", dan , i, dan * i));
+		}
+	}
+
+}
